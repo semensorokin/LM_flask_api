4ba2a9d967a0f918c1f038def572af535b869077
semensorokin <you@example.com>
2020-02-05 19:14:44 +0300

    fix strings

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pickle
+
 
 class myBert():
 
@@ -15,4 +17,16 @@ class myBert():
         for head in range(12):
             result['head_#'+str(head)] = np.random.rand(len(self.words),len(self.words)).tolist()
         return result
+
+
+#class myHMM():
+#    def __init__(self):
+#        self.model = pickle.load(open('cprob_nkra', 'rb'))
+#        
+#    def get_propobabilities(self, sentence):
+#        self.words = sentence.split(' ')
+#        sentence_probs = {self.words[0]: 0}
+#        for indx, word in enumerate(self.words[:-1])
+#            sentence_probs[word] = self.model[word].prob(self.words[indx+1])
+#        return setence_probs
         
